93012dd1b08eaa81061da1f5accb256c3bc633fd
watanuki2006 <watanuki2006@gmail.com>
2018-04-08 22:28:59 +0800

    Changes to be committed: 	modified:   root/script/git.account 	modified:   root/script/install_package_git.sh

--- root/script/install_package_git.sh
+++ root/script/install_package_git.sh
@@ -7,9 +7,19 @@ install_package git
 if [ -f git.account ]
 then
     name=`sed -n '1p' git.account`
+    if [ $name == "user.name" ]
+    then
+	echo "Please update git.account..."
+	exit -1
+    fi
     git config --global user.name "$name"
 
     email=`sed -n '2p' git.account`
+    if [ $email == "user.email" ]
+    then
+	echo "Please update git.account..."
+	exit -1
+    fi
     git config --global user.email "$email"
 
     git config --global core.editor emacs
