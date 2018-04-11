426b51bddc5ad5cb310371e016283adc26215f10
watanuki2006 <watanuki2006@gmail.com>
2018-04-11 23:58:04 +0800

    Changes to be committed: 	modified:   root/script/install_package_jekyll.sh

--- root/script/install_package_jekyll.sh
+++ root/script/install_package_jekyll.sh
@@ -16,6 +16,17 @@ gem install bundler > /dev/null 2>&1
 
 gem install minima > /dev/null 2>&1
 
+##################################################################
+# Example [of temp]
+##################################################################
+# WARNING:  You don't have /root/.gem/ruby/2.5.0/bin in your PATH,
+#           gem executables will not run.
+# Successfully installed jekyll-3.7.3
+# Parsing documentation for jekyll-3.7.3
+# Done installing documentation for jekyll after 0 seconds
+# 1 gem installed
+##################################################################
+
 for path_of_jekyll in `cat temp | grep "WARNING" | awk '{print $5}'`
 do
     break
@@ -27,6 +38,11 @@ cat ~/.bash_profile | grep "ruby" > /dev/null
 if [ $? == 1 ]
 then
     echo "Update PATH..."
-    path_with_jekyll_enable="export PATH=$PATH:$path_of_jekyll"
-    sed -i "/xorg-xinit/ i $path_with_jekyll_enable\n" ~/.bash_profile
+    cat ~/.bash_profil | grep "xorg-xinit" > /dev/null
+    if [ $? == 1 ]
+    then	
+	echo "$path_with_jekyll_enable" >> ~/.bash_profile
+    else
+	sed -i "/xorg-xinit/ i $path_with_jekyll_enable\n" ~/.bash_profile
+    fi
 fi
